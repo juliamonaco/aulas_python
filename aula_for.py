db382a1ec33b2057b84ae979a5731c67badb7ede
Julia Monaco <50450544+juliamonaco@users.noreply.github.com>
2020-09-11 19:39:19 -0300

    Alterações feitas na atividade 4

--- aula_for.py
+++ aula_for.py
@@ -1,1 +1,41 @@
-print("teste")
+print("---------------------------------")
+print("Bem vindo ao jogo de adivinhação!")
+print("---------------------------------")
+
+numero_secreto=42
+total_tentativas = 10
+
+
+for rodada in range(1, total_tentativas + 1):
+    print("Tentativa {} de {}" .format(rodada, total_tentativas, "\n"))
+    chute = input("Digite o seu número: ")
+    print("--------------------------------------------------")
+    print("Você digitou", chute)
+    chute_2 = int(chute)
+    
+    print("--------------------------------------------------")
+
+
+
+    acertou = chute_2 == numero_secreto
+    menor = chute_2 < numero_secreto
+    maior = chute_2 > numero_secreto
+
+
+
+    if (acertou): 
+        print("Você acertou!\n \n")
+    else:
+        if(maior):
+            print("Voce errou, seu chute foi maior que o numero secreto")
+        elif(menor):
+            print("Voce errou, seu chute foi menor que o numero secreto")
+    
+    print("-------------------------------------------------- \n")
+        
+    rodada = rodada + 1
+   
+
+print("-----------")    
+print("Fim do jogo!")
+print("-----------")
